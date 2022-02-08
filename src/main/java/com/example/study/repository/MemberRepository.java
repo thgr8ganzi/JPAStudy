62f5953e21db85234f3dfb9663987781b8a2fd33
leejisoo <leejisoo@atnsgroup.com>
2022-02-08 16:59:05 +0900

    JPA study

--- src/main/java/com/example/study/repository/MemberRepository.java
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -12,7 +12,7 @@ import javax.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, JpaSpecificationExecutor {
 //    List<Member> findByUsernameAndGreaterThan(String username, int age);
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
@@ -57,5 +57,6 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String name);
+
 
 }
